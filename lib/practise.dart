c2b30daaa5fa20f7c84986581a1ba86fba7914b7
raunakpurohit <raunak.wscubetech@gmail.com>
2025-07-04 21:02:01 +0530

    conditional programming

--- lib/practise.dart
+++ lib/practise.dart
@@ -1,12 +1,24 @@
+import 'dart:io';
+
 void main () { ///a!=A
 
-  int s1 = add(11,5);
+  /*int s1 = add(11,5);
   int s2 = add(s1,20);
   int s3 = add(s2, 34);
-  add(s3, 21);
+  add(s3, 21);*/
 
+  print("Enter no1: ");
+  int no1 = int.parse(stdin.readLineSync() ?? "0");
+
+  stdout.write("Enter no2: ");
+  int no2 = int.parse(stdin.readLineSync() ?? "0");
+
+  int diff = sub(no1, no2);
+
+  stdout.write("The Difference b/w $no1 and $no2 is $diff");
 }
 
+///add (why we are required this function)
 int add(int no1, int no2){
   int sum;
   sum = no1 + no2;
@@ -20,6 +32,19 @@ int add(int no1, int no2){
 
   return sum;
 }
+
+int sub(int no1, int no2) => no1-no2;
+
+///TASK 1: Write a program to calculate the product of two numbers
+///TASK 2: Write a program to calculate the division of two numbers
+///TASK 3: Write a program to calculate the remainder of two numbers
+///TASK 4: Write a program to find the greatest of 3 no given
+///Task 5: Write a program to check if the no is ODD or EVEN
+///TASK 6: Write a program to check if the year is LEAP year or not
+/// Arithmetic operations (+ , - , * , /, %)
+
+///5%2 -> 1
+///5/2 -> 2.5
 
 ///void main() {
 //   int no1, no2, sum;
